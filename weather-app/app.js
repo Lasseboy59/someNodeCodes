540e5354bbe18891bafcec3a584e42c31123c2b9
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-02-05 13:01:30 +0200

    added geocode.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,7 @@
-const request = require('request');
+
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
 .options({
@@ -14,20 +16,4 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-let encodedAddress = decodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyClzADoCo8cRCYVc_VYOONVrprC5WCQ0aE`,
-    json: true
-}, (error, response, body) => {
-    if(error){
-        console.log('Unable to connect to google servers!')
-    } else if(body.status === 'ZERO_RESULTS'){
-        console.log('Unable to find that address.')
-    } else if(body.status === 'OK'){
-        // console.log(JSON.stringify(response, undefined, 2));
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
-    }
-});
+geocode.geocodeAddress(argv.address);
