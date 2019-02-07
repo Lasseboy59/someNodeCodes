d3cec3a84b56b5411a7f2c41cc9fda1f64727046
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-02-07 15:32:15 +0200

    organising testing into blocks with describe

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,31 +3,35 @@ const expect = require('expect');
 
 let app = require('./server').app
 
-it('should return a string response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
-            })
-        })
-        .end(done);
+describe('Server', () => {
+
+    describe('Get', () => {
+        it('should return a string response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    })
+                })
+                .end(done);
+        });
+    });
+
+    describe('Get/users', () => {
+        it('should test that your name is on the list', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Lasse',
+                        age: 25
+                    })
+                })
+                .end(done)
+        });
+    });
+
 });
-
-// test
-// status code === 200 asser 200
-// assert toInclude
-
-it('should test that your name is on the list', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Lasse',
-                age: 25
-            })
-        })
-        .end(done)
-})
